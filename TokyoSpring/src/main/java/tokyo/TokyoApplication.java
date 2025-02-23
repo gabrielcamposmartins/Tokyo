664b6b01125ee357537b31441db342b40587ac22
Gabriel Campos <gabrielcamposmartins99@gmail.com>
2025-02-23 00:22:41 -0300

    Projeto criado

---
+++ TokyoSpring/src/main/java/tokyo/TokyoApplication.java
@@ -0,0 +1,13 @@
+package tokyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokyoApplication.class, args);
+	}
+
+}
