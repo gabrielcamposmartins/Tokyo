cf316a2e8182ff104dd0f92b6ec2fc8cf92c89f5
Gabriel Campos <gabrielcamposmartins99@gmail.com>
2025-02-24 16:44:59 -0300

    criado projeto front

---
+++ tokyo-vue/src/components/ContasSelect.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+      <option selected>Selecione a conta</option>
+      <option v-for="conta in contas" :key="conta" :value="conta">{{conta}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContasSelect",
+  props: {
+    contas: Array,
+  },
+  data() {
+    return {
+      msg: "Hello, World!",
+    };
+  },
+  methods: {
+    test() {
+      alert("Hello, World!");
+    },
+  },
+};
+</script>
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ tokyo-vue/src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <ContasSelect :contas="contas"/>
+    <h1>{{ homeMessage }}</h1>
+    <button @click="test()" ></button>
+  </div>
+</template>
+<script>
+import ContasSelect from './ContasSelect.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    ContasSelect,
+  },
+  data() {
+    return {
+      homeMessage: 'Selecione uma conta para começar',
+      contas: ['001', '002'],
+    }
+  },
+  methods: {
+    test() {
+      alert('Hello, World!');
+    }
+  }
+}
+</script>
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
